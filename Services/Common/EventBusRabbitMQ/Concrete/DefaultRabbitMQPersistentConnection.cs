3e10a08b69b2c17ed81d79550bbd03691dabb42f
Doğukan Doğan <flptdogukan@hotmail.com>
2024-03-09 00:03:06 +0300

    RabbitMQPersistentConnection added

---
+++ Services/Common/EventBusRabbitMQ/Concrete/DefaultRabbitMQPersistentConnection.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBusRabbitMQ.Concrete
+{
+    public class DefaultRabbitMQPersistentConnection : IRabbitMQPersistentConnection
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private readonly int _retryCount;
+        private readonly ILogger<DefaultRabbitMQPersistentConnection> _logger;
+        private bool _disposed;
+
+        public DefaultRabbitMQPersistentConnection(IConnectionFactory connectionFactory, int retryCount, ILogger<DefaultRabbitMQPersistentConnection> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _retryCount = retryCount;
+            _logger = logger;
+        }
+
+        public bool IsConnected { 
+            get 
+            {
+                return _connection != null && _connection.IsOpen && !_disposed;
+            } 
+        
+        }
+
+        public IModel CreateModel()
+        {
+            if (!IsConnected)
+                throw new InvalidOperationException("No RabbitMQ connections are avaible to perform this action");
+
+            return _connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _disposed = true;
+            try
+            {
+                _connection.Dispose();
+            }
+            catch (IOException ex)
+            {
+                _logger.LogCritical(ex.ToString());
+            }
+        }
+
+        public bool TryConnect()
+        {
+            _logger.LogInformation("RabbitMQ client is trying to connect");
+
+            var policy = RetryPolicy.Handle<SocketException>().Or<BrokerUnreachableException>().WaitAndRetry(_retryCount,retryAttempt => TimeSpan.FromSeconds(Math.Pow(2,retryAttempt)),(ex,time) => 
+            {
+                _logger.LogWarning(ex, "RabbitMQ client could not connect after {Timeout}s ({ExceptionMessage})", $"{time.TotalSeconds:n1}", ex);
+            });
+
+            policy.Execute(() =>
+            {
+                _connection = _connectionFactory.CreateConnection();
+            });
+            if (IsConnected) 
+            {
+                _connection.ConnectionShutdown += OnConnectionShutDown;
+                _connection.CallbackException += OnCallbackException;
+                _connection.ConnectionBlocked += OnConnectionBlocked;
+                _logger.LogInformation("RabbitMQ client acquired a persistent connection to '{HostName}' and is subscribed to failure events",_connection);
+
+                return true;
+            }
+            else
+            {
+                _logger.LogCritical("FATAL ERROR: RabbitMQ connections could not be created and opened");
+                return false;
+            }
+        }
+
+        private void OnConnectionShutDown(object sender, ShutdownEventArgs e)
+        {
+            if (_disposed) return;
+            _logger.LogWarning("A RabbitMQ connection shut down.Trying to re-connect...");
+            TryConnect();
+        }
+
+        private void OnCallbackException(object sender, CallbackExceptionEventArgs e)
+        {
+            if (_disposed) return;
+            _logger.LogWarning("A RabbitMQ connection shut down.Trying to re-connect...");
+            TryConnect();
+        }
+
+        private void OnConnectionBlocked(object sender, ConnectionBlockedEventArgs e)
+        {
+            if (_disposed) return;
+            _logger.LogWarning("A RabbitMQ connection shut down.Trying to re-connect...");
+            TryConnect();
+        }
+    }
+}
