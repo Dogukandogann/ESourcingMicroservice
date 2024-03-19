06f71b4ea3e056a4d3a76fc7c4fb03f7f87efdb8
Dogukandogann <flptdogukan@hotmail.com>
2024-03-19 22:44:31 +0300

    User signup and jquery libraries added

--- Services/SourcingUI/ESourcing.UI/Controllers/HomeController.cs
+++ Services/SourcingUI/ESourcing.UI/Controllers/HomeController.cs
@@ -1,10 +1,22 @@
-﻿using ESourcing.UI.VievModel;
+﻿using ESourcing.Core.Entities;
+using ESourcing.UI.VievModel;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace ESourcing.UI.Controllers
 {
     public class HomeController : Controller
     {
+        public readonly UserManager<AppUser> _userManager;
+        public readonly SignInManager<AppUser> _signInManager;
+
+        public HomeController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,6 +29,45 @@ namespace ESourcing.UI.Controllers
         public IActionResult Login(LoginVM loginVM)
         {
             return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(AppUserVM signupModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser usr = new AppUser();
+                usr.FirstName = signupModel.FirstName;
+                usr.LastName = signupModel.LastName;
+                usr.Email = signupModel.Email;
+                usr.PhoneNumber = signupModel.PhoneNumber;
+                usr.UserName = signupModel.UserName;
+                if (signupModel.UserSelectTypeId==1)
+                {
+                    usr.IsBuyer = true;
+                    usr.IsSeller = false;
+                }
+                else
+                {
+                    usr.IsSeller= true;
+                    usr.IsBuyer = false;
+                }
+                var result = await _userManager.CreateAsync(usr,signupModel.Password);
+                if (result.Succeeded)
+                    return RedirectToAction("Login");
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("",error.Description);
+                    }
+                }
+            }
+            
+            return View(signupModel);
         }
     }
 }
